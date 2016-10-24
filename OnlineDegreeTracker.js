5d56f45f87a7de1c750ce0166687a514b3524a1d
ecastronovo <eric.castronovo@att.net>
2016-10-24 11:20:07 -0700

    Update OnlineDegreeTracker.js

--- OnlineDegreeTracker.js
+++ OnlineDegreeTracker.js
@@ -1,7 +1,7 @@
 var number = [];
 var advancedWriting = ["ENGL 181"];
 var CI1 = ["ANTH 11A", "ARTH 11A", "ARTH 11H", "CLAS 11A", "ENGL 11A", "ENVS 11A", "HIST 11A", "HIST 11H", "ITAL 11A", "MUSC 11A", "PHIL 11A", "SOCI 11A", "THTR 11A", "WGST 11A"];
-var cars = ["Saab", "Volvo", "BMW"];
+var CI2 = ["ANTH 12A", "ARTH 12A","CLAS 12A","ENGL 12A","ENVS 12A","HIST 12A","HIST 12H","ITAL 12A","MUSC 12A","PHIL 12A","SOCI 12A","THTR 12A","WGST 12A"];
 var cars = ["Saab", "Volvo", "BMW"];
 var cars = ["Saab", "Volvo", "BMW"];
 var cars = ["Saab", "Volvo", "BMW"];
