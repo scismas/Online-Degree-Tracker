8e68f9477bbcc0b7c2c79eca8088470a652ab9aa
ecastronovo <eric.castronovo@att.net>
2016-10-24 11:11:21 -0700

    Add files via upload

---
+++ OnlineDegreeTracker.js
@@ -0,0 +1,17 @@
+var number = [];
+var advancedWriting = ["ENGL 181"];
+var CI1 = ["ANTH 11A", "ARTH 11A", "ARTH 11H", "CLAS 11A", "ENGL 11A", "ENVS 11A", "HIST 11A", "HIST 11H", "ITAL 11A", "MUSC 11A", "PHIL 11A", "SOCI 11A", "THTR 11A", "WGST 11A"];
+var cars = ["Saab", "Volvo", "BMW"];
+var cars = ["Saab", "Volvo", "BMW"];
+var cars = ["Saab", "Volvo", "BMW"];
+var cars = ["Saab", "Volvo", "BMW"];
+var cars = ["Saab", "Volvo", "BMW"];
+var cars = ["Saab", "Volvo", "BMW"];
+
+
+function myFunction()
+{
+  var x = document.getElementById("box");
+  number.push(document.getElementById("input").value);
+  x.innerHTML = number.join('<br/>');
+}
