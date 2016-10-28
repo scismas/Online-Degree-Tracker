cdeef3e31a20cdc5a6b65109d78c506b99e4da8e
scismas <sabrinaac@gmail.com>
2016-10-28 16:12:11 -0700

    Add files via upload

--- OnlineDegreeTracker.js
+++ OnlineDegreeTracker.js
@@ -45,7 +45,6 @@ function removeCourse() {
     courses.splice(i, 1);
   }
 }
-
 */
 //Would like case insenitive
 function addCourse(){
@@ -60,6 +59,7 @@ function addCourse(){
     x.innerHTML += courses[i] + "<br>";
     generate(courses[i]);
   }
+  createCookie();
 }
 
 function generate(x){
@@ -96,5 +96,37 @@ function generate(x){
   else if(x == "ELEN 153"){document.getElementById("ELEN 153").innerHTML = "YES!";}
  
 }
-  
 
+var createCookie = function(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
+
+
+
+
+  
